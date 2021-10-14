1a133185cafe081e71e1a51424b0cd0685ef1229
Sabrina Sergio <sabrinamidori@gmail.com>
2021-10-14 15:47:51 -0300

    Add users index action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
     before_action :set_user, only: %i[ show edit update destroy ]
+    def index 
+        @users = User.all 
+    end
     def new
          @user = User.new
     end
