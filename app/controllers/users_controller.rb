2ef291d5e6e47513aa66c9342ac42b11704755f2
Sabrina Sergio <sabrinamidori@gmail.com>
2021-10-14 11:13:06 -0300

    Add users create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,28 @@
 class UsersController < ApplicationController
+    #before_action :set_user, only: %i[ show edit update destroy ]
     def new
          @user = User.new
     end
+    def create
+        #byebug
+        @user = User.new(user_params) 
     
+        if @user.save
+            flash[:notice] = "Wellcome to Alpha Blog #{@user.username}, you have successfully signed up."
+            redirect_to articles_path
+        else
+            render 'new'  
+        end
+    end
+    
+    private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
 end
